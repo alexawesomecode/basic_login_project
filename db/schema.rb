32c17e57c214170fa99856e4c2edfebffac386f0
alexawesomecode <alexawesomecode@gmail.com>
2019-12-13 18:38:56 -0300

    created remember cookies and sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_203522) do
+ActiveRecord::Schema.define(version: 2019_12_13_205453) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,7 +19,8 @@ ActiveRecord::Schema.define(version: 2019_12_12_203522) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "remember_digest"
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
